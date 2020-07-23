f6e5b658f7a9cca32ea222643da5f7ca0b371124
James Larivee <mr_15th_entertainer@yahoo.com>
2020-07-22 18:18:20 -0600

    Added transaction page. New TODO added.

---
+++ app/src/main/java/io/jabooty/BlankViewModel.java
@@ -0,0 +1,7 @@
+package io.jabooty;
+
+import androidx.lifecycle.ViewModel;
+
+public class BlankViewModel extends ViewModel {
+  // TODO: Implement the ViewModel
+}
--- app/src/main/java/io/jabooty/ui/home/HomeSecondFragment.java
+++ app/src/main/java/io/jabooty/ui/home/HomeSecondFragment.java
@@ -1,16 +1,24 @@
 package io.jabooty.ui.home;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import android.widget.TextView.OnEditorActionListener;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import io.jabooty.R;
+import java.util.ArrayList;
 
 public class HomeSecondFragment extends Fragment {
 
@@ -25,16 +33,29 @@ public class HomeSecondFragment extends Fragment {
 
   public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-
     String myArg = HomeSecondFragmentArgs.fromBundle(getArguments()).getMyArg();
-    TextView textView = view.findViewById(R.id.textview_home_second);
-    textView.setText(getString(R.string.hello_home_second, myArg));
+    final ListView listView = view.findViewById(R.id.listView);
+    final TextView textView = view.findViewById(R.id.textview_home_second);
+    final ArrayList<String> arrayList = new ArrayList<String>();
+    final EditText editText = view.findViewById(R.id.editText);
 
     view.findViewById(R.id.button_home_second).setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
         NavHostFragment.findNavController(HomeSecondFragment.this)
             .navigate(R.id.action_HomeSecondFragment_to_HomeFragment);
+      }
+    });
+
+    editText.setOnEditorActionListener(new OnEditorActionListener() {
+      @Override
+      public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if(!editText.getText().toString().equals("")){
+          editText.setText("");
+          arrayList.add(editText.getText().toString());
+        }
+        //TODO Add arrayList values to a scrolling list to be displayed in the app.
+        return false;
       }
     });
   }
